481de891affbdee8e1e4db78207002514d3388cb
amithgopi <amith314@gmail.com>
2022-03-11 17:30:11 -0600

    Added all files

--- gopi_proj2_list.c
+++ gopi_proj2_list.c
@@ -1,18 +1,42 @@
+/**
+ * @file list.c
+ * @author Amith Gopi (amithgopi@tamu.edu)
+ * @brief Implement a simple doubly linked list for integer type values
+ * @version 1.0
+ * @date 2022-03-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Acts as a node in the struct. Holds the value and the next and previous pointers
+ * 
+ */
 struct Node{
     int pageFrame;
     struct Node *next;
     struct Node *previous;
 } Node;
 
+/**
+ * @brief Wrapper struct for the list. Contains the front and back pointers and size of list.
+ * 
+ */
 struct List{
     struct Node *front;
     struct Node *back;
     int size;
 } ;
 
+/**
+ * @brief Add a entry to the front of the queue
+ * 
+ * @param list Pointer to the list
+ * @param page Page number
+ */
 void push_front(struct List* list, int page) {
     struct Node *n = malloc(sizeof(struct Node));
     n->pageFrame = page; n->next= list->front; n->previous = NULL;
@@ -22,6 +46,12 @@ void push_front(struct List* list, int page) {
     list->size++;
 }
 
+/**
+ * @brief Pop an entry from the back of the list
+ * 
+ * @param list Pointer to the list
+ * @return int Value of the element removed, if null return -1
+ */
 int pop_back(struct List* list) {
     if (list->back == NULL) {
         return -1;
@@ -43,6 +73,13 @@ int pop_back(struct List* list) {
     }
 }
 
+/**
+ * @brief Find the pointer to the given page number in the list
+ * 
+ * @param list Pointer to the list
+ * @param page page number to search for
+ * @return struct Node* Poiner to first element in the list where the page is located, NULL is not found
+ */
 struct Node* find(struct List *list, int page) {
     struct Node* curr = list->front;
     int location = 0;
@@ -54,12 +91,26 @@ struct Node* find(struct List *list, int page) {
     }
 }
 
+/**
+ * @brief Find a given page in the list and rmeove its corresponding entry
+ * 
+ * @param list Pointer to the list
+ * @param page page number to search for
+ * @return int value of the page beind removed, else return -1
+ */
 int remove_page(struct List* list, int page) {
     struct Node* n = find(list, page);
     int val;
     val = remove_element(list, n);
 }
 
+/**
+ * @brief Remove the node given by its pointer from the list
+ * 
+ * @param list Pointer to the list
+ * @param n Pointer to the node
+ * @return int value of the page beind removed, else return -1
+ */
 int remove_element(struct List* list, struct Node* n) {
     if(n == NULL) {
         return -1;
@@ -78,14 +129,31 @@ int remove_element(struct List* list, struct Node* n) {
     }
 }
 
+/**
+ * @brief Get the value of entry at back of the list
+ * 
+ * @param list Pointer to the list
+ * @return int value of entry at back of the list
+ */
 int get_back(struct List* list) {
     return list->back->pageFrame;
 }
 
+/**
+ * @brief Get the value of entry at front of the list
+ * 
+ * @param list Pointer to the list
+ * @return int value of entry at back of the list
+ */
 int get_front(struct List *list){
     return list->front->pageFrame;
 }
 
+/**
+ * @brief Print out the elements of the given list in the a formatted manner
+ * 
+ * @param list 
+ */
 void print_list(struct List* list) {
     struct Node* curr = list->front;
     while(curr != NULL) {
--- gopi_proj2_main.c
+++ gopi_proj2_main.c
@@ -1,14 +1,27 @@
-#include "list.c"
+/**
+ * @file gopi_proj2_main.c
+ * @author Amith Gopi (amithgopi@tamu.edu) (431000927)
+ * @brief CSCE611 Project 2. Complie using `gcc gopi_proj2_main.c -o gopi_proj2.exe -lm`
+ * @version 1.0
+ * @date 2022-03-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "gopi_proj2_list.c"
 #include <stdbool.h>
 #include <time.h>
 #include <sys/time.h>
+#include <math.h>
 
-#define INPUT_FILENAME "input.txt"
-#define OUTPUT_FILENAME_4FRAMES "output_4 frames.txt"
-#define OUTPUT_FILENAME_8FRAMES "output_8 frames.txt"
+#define INPUT_FILENAME "gopi_proj2_input.txt"
+#define OUTPUT_FILENAME_4FRAMES "gopi_proj2_output_4 frames.txt"
+#define OUTPUT_FILENAME_8FRAMES "gopi_proj2_output_8 frames.txt"
 #define INPUT_LENGTH 100
 #define NUMBER_OF_PAGES 16
 #define TIMESTAMP_FORMAT "%02d/%02d/%04d %02d:%02d:%02d:%03d"
+#define STANDARD_DEVIATION 2.5
+#define ENABLE_LOGGING_PAGEFRAME_LIST 0 // Set to 1 to show the page frame queue in stdout
 
 #define FOREACH_ALGORITHM(ALGORITHM) \
         ALGORITHM(FIFO)   \
@@ -24,57 +37,50 @@ static const char *ReplacementAlgorithmStrings[] = {
     FOREACH_ALGORITHM(GENERATE_STRING)
 };
 
+//Global paramters
 int numPageFrames = 4;
 int *pageFault;
 char *outputFileName = OUTPUT_FILENAME_4FRAMES;
 
-
+//Function declarations
 void createInputFile();
 int getUserInput();
 int getPageFromInputFile(FILE*);
-void runReplacementAlgorithm(enum ReplacementAlgorithms, FILE*);
+void setNumberOfFrames();
+void runReplacementAlgorithm(int, FILE*);
+
 int runFIFO(struct List*, struct List*, int);
 int runLRU(struct List*, struct List*, int);
 int runSecondChance(struct List*, struct List*, int, int*);
-void setNumberOfFrames();
 
+/**
+ * @brief Main function
+ * 
+ * @return int 
+ */
 int main() {
 
     int userChoice = 0;
     enum ReplacementAlgorithms userChosenAlgorithm;
-
+    // Seed rand
     srand(time(0));
+    // Create input file if not present
     createInputFile();
-
-    FILE* inputFilePtr = fopen(INPUT_FILENAME, "r");
-    
-    while (userChoice == 0) {
+    // Loop till exit
+    while(1) {
+        // Open input file
+        FILE* inputFilePtr = fopen(INPUT_FILENAME, "r");
+        // Get user input for algorithm
         userChoice = getUserInput();
+        // Check exit condition
+        if (userChoice == 4) break;
+        // Get number of page frames from user
+        setNumberOfFrames();
+        // Run the selected algorithm on input
+        runReplacementAlgorithm(userChoice, inputFilePtr);
+        //Close input file
+        fclose(inputFilePtr);
     }
-
-    switch (userChoice)
-    {
-        case 1:
-            printf("Selected - FIFO\n");
-            userChosenAlgorithm = FIFO;
-            break;
-        case 2:
-            printf("Selected - LRU\n");
-            userChosenAlgorithm = LRU;
-            break;
-        case 3:
-            printf("Selected - Second Chance\n");
-            userChosenAlgorithm = SECOND_CHANCE;
-            break;
-        default:
-            break;
-    }
-
-    setNumberOfFrames();
-
-    runReplacementAlgorithm(userChosenAlgorithm, inputFilePtr);
-
-    fclose(inputFilePtr);
     return 0;
 }
 
@@ -89,7 +95,7 @@ void setNumberOfFrames() {
     } while (numPageFrames != 4 && numPageFrames != 8);
 
     printf("Selected memory with %d page frames.\n", numPageFrames);
-
+    // Set output fileName
     if(numPageFrames == 4) {
         outputFileName = OUTPUT_FILENAME_4FRAMES;
     } else {
@@ -97,12 +103,24 @@ void setNumberOfFrames() {
     }
 }
 
+/**
+ * @brief Read one line of input from input file
+ * This would give the number of the next page to take as input
+ * 
+ * @param filePtr 
+ * @return int 
+ */
 int getPageFromInputFile(FILE *filePtr) {
     int page = -1;
     fscanf(filePtr, "%d\n", &page);
     return page;
 }
 
+/**
+ * @brief Generate the current system timestanp and format it as a string
+ * 
+ * @return char* 
+ */
 char* getTimeStamp() {
     struct timeval currentTime;
     gettimeofday(&currentTime, NULL);
@@ -112,6 +130,14 @@ char* getTimeStamp() {
     return timeStamp;
 }
 
+/**
+ * @brief Print thr formatted output for each page input, to the output file with timestamp, page and page fault info.
+ * This also prints the same string to stdout.
+ * 
+ * @param filePtr File pointer for the output file
+ * @param page Input page
+ * @param fault 1 if page gets a fault else 0
+ */
 void printPageStatusToFile(FILE* filePtr, int page, int fault) {
     char pageStatusString[50];
     snprintf(pageStatusString, 50, "%s %02d %s\n", getTimeStamp(), page, fault ? "FAULT" : "HIT");
@@ -119,75 +145,104 @@ void printPageStatusToFile(FILE* filePtr, int page, int fault) {
     printf(pageStatusString);
 }
 
-void runReplacementAlgorithm(enum ReplacementAlgorithms algorithm, FILE* filePtr) {
+/**
+ * @brief Run the selected algorithm in the input file and generate the output
+ * 
+ * @param algorithmChoice User choice of the algorithm
+ * @param filePtr Pointer to input file
+ */
+void runReplacementAlgorithm(int algorithmChoice, FILE* filePtr) {
+    // Init variables
     int page = NUMBER_OF_PAGES + 1; // Init page to a value not possible in the input list that is not -1
-    int fault;
+    int fault;  //Indicate if current page genrates a fault
     int faultCount = 0;
-
+    // Open output file
     FILE *outputPtr = fopen(outputFileName, "a");
-    fprintf(outputPtr, "Running %s with %d Page frames\n", ReplacementAlgorithmStrings[algorithm], numPageFrames);
-
+    //Print output header
+    fprintf(outputPtr, "Running %s with %d Page frames\n", ReplacementAlgorithmStrings[algorithmChoice-1], numPageFrames);
+    //Init lists/queues to store page frames
     struct List *memory = malloc(sizeof(struct List));
     memory->front = NULL;
     memory->back = NULL;
+    memory->size = 0;
     struct List *recent = malloc(sizeof(struct List));
     recent->front = NULL;
     recent->back = NULL;
+    recent->size = 0;
+    // Array to hold the referenced bit
     int referenced[NUMBER_OF_PAGES] = {[0 ... (NUMBER_OF_PAGES-1)] = 0};
-
+    // Loop over all entries in the input
     for(int i=0; i<INPUT_LENGTH; i++) {
         page = getPageFromInputFile(filePtr);
         if(page == -1) break;
-               
-        switch (algorithm) {
-            case FIFO:
+        // selct algorithm to run based on user input     
+        switch (algorithmChoice) {
+            case 1:
                 fault = runFIFO(memory, recent, page);
                 break;
-            case LRU:
+            case 2:
                 fault = runLRU(memory, recent, page);
                 break;
-            case SECOND_CHANCE:
+            case 3:
                 fault = runSecondChance(memory, recent, page, referenced);
                 break;
             default:
                 break;
         }
-        
+        // Increment number of faults if page added creates a fault  
         if(fault) faultCount++;
+        // Print the log for the current page
         printPageStatusToFile(outputPtr, page, fault);
-        print_list(memory);
+        if(ENABLE_LOGGING_PAGEFRAME_LIST) print_list(memory);
     }
+    // Print details of total number of faults in the run
     fprintf(outputPtr, "Total Number of Faults: %d", faultCount);
     fprintf(outputPtr, "\n\n");
     printf("\n");
-
+    // Free resources
     fclose(outputPtr);
     free(memory);
     free(recent);
 }
 
 
-
+/**
+ * @brief Get user input to choose a replacement algorithm
+ * 
+ * @return int 
+ */
 int getUserInput() {
     int userChoice = 0;
     printf("1. FIFO\n2. LRU\n3. Second Chance\n4. Exit\n");
     printf("Choose a page replacemnt algorithm (1, 2, or 3): ");
     scanf("%d", &userChoice);
-    if(userChoice <= 0 || userChoice > 4) {
+    while(userChoice == 0) {
+        if(userChoice <= 0 || userChoice > 4) {
         printf("Invaid Choice\n");
         userChoice = 0;
+        }
     }
     return userChoice;
-
 }
 
+/**
+ * @brief Create an input file with a random sequence of integers from [0, NUMBER_OF_PAGES-1]. The file is created
+ * if it does not exist, else do nothing
+ * 
+ */
 void createInputFile() {
     FILE *filePtr = fopen(INPUT_FILENAME, "r");
     // Check if file does not exist
     if (!filePtr) {
         filePtr = fopen(INPUT_FILENAME, "w");
         for(int i=0; i<INPUT_LENGTH; i++) {
-            fprintf(filePtr, "%d\n", (int)((rand() / (RAND_MAX + 1.0)) * (NUMBER_OF_PAGES)) );
+            // Generate normally distributed random variable with mean (NUMBER_OF_PAGES-1)/2 = 7.5 and standard deviation = STANDARD_DEVIATION
+            float r1 = ((rand() / (RAND_MAX + 1.0)));
+            float r2 = ((rand() / (RAND_MAX + 1.0)));
+            float normalRand = sqrt(-2.0*log(r1))*cos(2*3.14159*r2)*STANDARD_DEVIATION + (NUMBER_OF_PAGES-1)/2.0;
+            if (normalRand < 0.5) normalRand = 0;
+            if (normalRand > NUMBER_OF_PAGES-1.5) normalRand = NUMBER_OF_PAGES-1;
+            fprintf(filePtr, "%d\n", (int)round(normalRand) );
         }
     }
     fclose(filePtr);
